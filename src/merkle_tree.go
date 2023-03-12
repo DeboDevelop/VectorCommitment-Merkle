f960b96e51c978a515edd91ff9b6fc5544e30084
DeboDevelop <debajyotidofficial@gmail.com>
2023-03-12 15:14:01 +0530

    feat: Add parent in merkle tree node

--- src/merkle_tree.go
+++ src/merkle_tree.go
@@ -10,10 +10,11 @@ import (
 )
 
 type Node struct {
-	key   string
-	hash  string
-	left  *Node
-	right *Node
+	key    string
+	hash   string
+	left   *Node
+	right  *Node
+	parent *Node
 }
 
 func hashContent(dataList []string) []Node {
@@ -21,7 +22,7 @@ func hashContent(dataList []string) []Node {
 	for _, data := range dataList {
 		hash := sha256.New()
 		hash.Write([]byte(data))
-		hashedArr = append(hashedArr, Node{data, hex.EncodeToString(hash.Sum(nil)), nil, nil})
+		hashedArr = append(hashedArr, Node{data, hex.EncodeToString(hash.Sum(nil)), nil, nil, nil})
 	}
 	return hashedArr
 }
@@ -34,7 +35,9 @@ func buildTree(dataList []string) Node {
 		for i < len(hashedArr) {
 			hash := sha256.New()
 			hash.Write([]byte(hashedArr[i-1].hash + hashedArr[i].hash))
-			newNode := Node{hashedArr[i-1].key + hashedArr[i].key, hex.EncodeToString(hash.Sum(nil)), &hashedArr[i-1], &hashedArr[i]}
+			newNode := Node{hashedArr[i-1].key + hashedArr[i].key, hex.EncodeToString(hash.Sum(nil)), &hashedArr[i-1], &hashedArr[i], nil}
+			hashedArr[i-1].parent = &newNode
+			hashedArr[i].parent = &newNode
 			hashedTreeLeaf = append(hashedTreeLeaf, newNode)
 			i = i + 2
 		}
@@ -58,7 +61,6 @@ func inorder(root Node) {
 
 func getWitnessProver(key string, root Node) ([]Node, error) {
 	keys := strings.Split(key, "/")
-	fmt.Println(keys)
 	var parent, leftChild, rightChild Node
 	witnesses := make([]Node, 0)
 	for i, nodeKey := range keys {
