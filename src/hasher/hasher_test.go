e10ba84dab65ff0fba02004533f60426629b0324
DeboDevelop <debajyotidofficial@gmail.com>
2023-04-05 23:18:02 +0530

    feat: Add unit test for hasher

--- src/hasher/hasher_test.go
+++ src/hasher/hasher_test.go
@@ -1,7 +1,20 @@
 package hasher_test
 
-import "testing"
+import (
+	"encoding/hex"
+	"testing"
 
-func TestRandom(t *testing.T) {
+	"github.com/DeboDevelop/MerkleProofVerifier/hasher"
+)
 
+func TestHashers(t *testing.T) {
+	t.Run("Test SHA256", func(t *testing.T) {
+		input := []byte("1")
+		got := hex.EncodeToString(hasher.SHA256Hasher(input))
+		want := "6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b"
+
+		if got != want {
+			t.Errorf("got %v want %v given, %v", got, want, input)
+		}
+	})
 }
